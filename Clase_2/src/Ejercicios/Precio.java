be24a5dc15d5c3f5b6bd9de9a84c314bee9577f9
Gerson Santos <gersantos1234@gmail.com>
2018-10-12 10:22:54 -0500

    Agregando ejercicios en clase

---
+++ Clase_2/src/Ejercicios/Precio.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicios;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Precio {
+    double euros;
+    public double obtener (){
+        return 0;
+    }
+    public void agregar (double x){
+        
+    }
+}
---
+++ Clase_2/src/Ejercicios/Principal.java
@@ -0,0 +1,24 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Precio p;
+        p = new Precio();
+        System.out.println(p.euros);
+        
+    }
+    
+}
